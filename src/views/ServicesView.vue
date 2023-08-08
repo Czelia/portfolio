34abc40c5a8f26aca08dad30cf40fe2130fcd6e6
Salvino V <salvino.varciat@gmail.com>
2023-08-08 13:47:38 -0400

    fix font

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -2,7 +2,7 @@
 	<div>
 		<h2 class="opacity-70 uppercase">THE DIGITAL MAGIC</h2>
 		<h1 class="uppercase">CAPTIVATING DESIGN, <br />POWERFUL DEVELOPMENT.</h1>
-		<p class="text-sm mt-4">
+		<p class="font-serif mt-4">
 			Unleash the power of captivating UI design and web development. From
 			visually stunning interfaces to seamless user experiences, I bring your
 			vision to life with creativity and technical expertise. Let's collaborate
